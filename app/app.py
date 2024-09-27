eceeb1b8da284ec59fd38be9cbc010623f93c68e
Giuseppe La Tosa <g.latosa@studenti.unipi.it>
2024-09-27 14:14:20 +0200

    Aggiunta della nuova funzionalità nel file.py

--- app/app.py
+++ app/app.py
@@ -1,4 +1,5 @@
 from flask import Flask, request, make_response, jsonify
+import random
 
 app = Flask(__name__, instance_relative_config=True)
 
@@ -6,20 +7,62 @@ app = Flask(__name__, instance_relative_config=True)
 def add():
     a = request.args.get('a', type=float)
     b = request.args.get('b', type=float)
-    if a and b:
-        return make_response(jsonify(s=a+b), 200) #HTTP 200 OK
+    if a is not None and b is not None:
+        return make_response(jsonify(s=a+b), 200)  # HTTP 200 OK
     else:
-        return make_response('Invalid input\n', 400) #HTTP 400 BAD REQUEST
+        return make_response('Invalid input\n', 400)  # HTTP 400 BAD REQUEST
 
-#Endpoint /sub for subtraction which takes a and b as query parameters.
+@app.route('/sub')
+def sub():
+    a = request.args.get('a', type=float)
+    b = request.args.get('b', type=float)
+    if a is not None and b is not None:
+        return make_response(jsonify(s=a-b), 200)  # HTTP 200 OK
+    else:
+        return make_response('Invalid input\n', 400)  # HTTP 400 BAD REQUEST
 
-#Endpoint /mul for multiplication which takes a and b as query parameters.
+@app.route('/mul')
+def mul():
+    a = request.args.get('a', type=float)
+    b = request.args.get('b', type=float)
+    if a is not None and b is not None:
+        return make_response(jsonify(s=a*b), 200)  # HTTP 200 OK
+    else:
+        return make_response('Invalid input\n', 400)  # HTTP 400 BAD REQUEST
 
-#Endpoint /div for division which takes a and b as query parameters. Returns HTTP 400 BAD REQUEST also for division by zero.
+@app.route('/div')
+def div():
+    a = request.args.get('a', type=float)
+    b = request.args.get('b', type=float)
+    if a is not None and b is not None:
+        if b == 0:
+            return make_response('Division by zero\n', 400)  # HTTP 400 BAD REQUEST
+        return make_response(jsonify(s=a/b), 200)  # HTTP 200 OK
+    else:
+        return make_response('Invalid input\n', 400)  # HTTP 400 BAD REQUEST
 
-#Endpoint /mod for modulo which takes a and b as query parameters. Returns HTTP 400 BAD REQUEST also for division by zero.
+@app.route('/mod')
+def mod():
+    a = request.args.get('a', type=float)
+    b = request.args.get('b', type=float)
+    if a is not None and b is not None:
+        if b == 0:
+            return make_response('Division by zero\n', 400)  # HTTP 400 BAD REQUEST
+        return make_response(jsonify(s=a % b), 200)  # HTTP 200 OK
+    else:
+        return make_response('Invalid input\n', 400)  # HTTP 400 BAD REQUEST
 
-#Endpoint /random which takes a and b as query parameters and returns a random number between a and b included. Returns HTTP 400 BAD REQUEST if a is greater than b.
+@app.route('/random')
+def random_number():
+    a = request.args.get('a', type=int)
+    b = request.args.get('b', type=int)
+    if a is not None and b is not None:
+        if a > b:
+            return make_response('Invalid input: a should not be greater than b\n', 400)  # HTTP 400 BAD REQUEST
+        random_num = random.randint(a, b)
+        return make_response(jsonify(random=random_num), 200)  # HTTP 200 OK
+    else:
+        return make_response('Invalid input\n', 400)  # HTTP 400 BAD REQUEST
 
 if __name__ == '__main__':
     app.run(debug=True)
